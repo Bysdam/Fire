977b8cc18685754225e8d8763f61c2c6f02e9eca
Basil Mudathir <basilali7777@gmail.com>
2025-07-19 15:01:29 +0800

    remove system config functions

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -46,7 +46,7 @@
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
+
 /* USER CODE BEGIN PFP */
 void gpio_init();
 void USART1_init(void);
@@ -86,7 +86,7 @@ USART1_init();
   /* USER CODE END Init */
 
   /* Configure the system clock */
-  SystemClock_Config();
+  //SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
 
@@ -96,7 +96,26 @@ USART1_init();
   /* USER CODE BEGIN 2 */
 gpio_init();
 tick = HAL_GetTick();
-//motor_forward();
+
+void tim1_config() {
+    RCC->APB2ENR |= (1 << 11);     // Enable TIM1 clock
+    TIM1->PSC = 72 - 1;            // Prescaler for 1 MHz timer (assuming 72 MHz system clock)
+    TIM1->ARR = 0xFFFF;            // Set auto-reload register to max value
+    TIM1->CR1 |= (1 << 0);         // Enable TIM1
+}
+
+// Microsecond delay using TIM1
+void Delay_us(uint16_t us) {
+    TIM1->CNT = 0;                 // Reset counter
+    while (TIM1->CNT < us);       // Wait until desired time has passed
+}
+
+// Millisecond delay based on Delay_us
+void Delay_ms(uint16_t ms) {
+    for (uint16_t i = 0; i < ms; i++) {
+        Delay_us(1000);           // 1 millisecond delay
+    }
+}
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -135,42 +154,8 @@ tick = HAL_GetTick();
   * @brief System Clock Configuration
   * @retval None
   */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
-    Error_Handler();
-  }
-}
 
 /* USER CODE BEGIN 4 */
 void gpio_init()
@@ -308,34 +293,4 @@ void USART1_init(void){
 }
 /* USER CODE END 4 */
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
-}
 
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
